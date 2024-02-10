aea405a33462a04984bc72611e6ebdacaa5ce6cb
Jeef <jeeftheskroob@gmail.com>
2024-02-10 13:53:17 -0600

    fixed

--- passives/overgrowth.gd
+++ passives/overgrowth.gd
@@ -1,9 +1,9 @@
 extends Buff
 func apply(player):
-    player.max_health *= 1.25
-    player.health *= 1.25
+	player.max_health *= 1.25
+	player.health *= 1.25
 
 func remove(player):
-    player.max_health *= 0.8
-    player.health = clamp(0.8 * health, 1, max_health)
-    #clamping to make sure the player doesnt die from unequipping it, not sure if necessary
+	player.max_health *= 0.8
+	player.health = clamp(0.8 * health, 1, max_health)
+	#clamping to make sure the player doesnt die from unequipping it, not sure if necessary
